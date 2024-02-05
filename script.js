1986ec60f16ab17258ad918f0b1fb86153eaa680
Rukaiya Bano <rukaiyabano888@gmail.com>
2024-02-05 07:18:48 -0800

    improve codes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector(".btn");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to    select");
+const msg = document.querySelector(".convert-msg");
 
 
 
@@ -24,6 +25,22 @@ for (select of dropdowns){
     });
 }
 
+const updateExchangeRate = async () =>{
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+};
+
+
 const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
@@ -34,16 +51,10 @@ const updateFlag = (element) => {
 
 btn.addEventListener("click", (evt) =>{
     evt.preventDefault();
-    let amount = document.querySelector(".amount input");
-    let amtVal = amount.value;
-    if(amtVal === "" || amtVal < 1){
-        amtVal = 1;
-        amount.value = "1";
-    }
-    
+    updateExchangeRate();
 });
 
-// console.log(fromCurr.value, toCurr.value);
 
-const URL = `${BASE_URL}/${fromCurr.value.toLowerCase}/${toCurr.value.toLowerCase}.json`
-
+window.addEventListener("load", () =>{
+    updateExchangeRate();
+});
